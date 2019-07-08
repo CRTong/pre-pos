f38ccce3d501d838bcf536eb8e2b984f6cd89f5d
CRTong <1848126828@qq.com>
2019-07-08 23:56:22 +0800

    section2 and section3

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,29 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const result = {};
+  const keys = [];
+  const pushItem = (key, value) => {
+    !keys.includes(key) && keys.push(key);
+    result[key] = result[key] ? result[key] + parseInt(value) : parseInt(value);
+  };
+  const splitKeys = [''];
+
+  const matchSplitKey = str => {
+    const matchKeys = splitKeys.filter(item => str.includes(item));
+    return matchKeys.length > 0 ? matchKeys[0] : undefined;
+  };
+  
+  for(var i in collection){
+    var temp = collection[i];
+    const flag = matchSplitKey(temp);
+    if(flag){
+      const [key,value] = temp.split(flag);
+      pushItem(key,value);
+    }else{
+      pushItem(temp,1);
+    }
+  }
+
+  return keys.map(temp => ({ key: temp, count: result[temp] }));
 }
--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,11 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+  for(var i = 0,l = collectionA.length;i < l;i++){
+    if(objectB['value'].indexOf(collectionA[i].key) != -1){
+      collectionA[i].count = collectionA[i].count - parseInt(collectionA[i].count / 3);
+    }
+  }
+  return collectionA;
 }
